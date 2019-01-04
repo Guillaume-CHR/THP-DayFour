f0b7212744fff6cfcf9d90c040e61b6a27eafa65
Guillaume_CH <guillaume.christe@gmail.com>
2019-01-04 01:31:55 +0100

    exo_07_a.rb

--- THP-exercice.rb
+++ THP-exercice.rb
@@ -1,6 +1,3 @@
-number_of_hours_worked_per_day = 10
-number_of_days_worked_per_week = 5
-number_of_weeks_in_THP = 11
-
-puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
-puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
