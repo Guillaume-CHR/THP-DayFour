b6aa9dc1bc19ab77d82ad3d10bbdd5accb638c3e
Guillaume_CH <guillaume.christe@gmail.com>
2019-01-04 11:25:08 +0100

    exo_17.rb

--- THP-exercice.rb
+++ THP-exercice.rb
@@ -3,5 +3,9 @@ print "> "
 iAge = gets.chomp.to_i
 
 for _iNum in (0..iAge)
-	puts "Il y a #{_iNum} ans, tu avais #{iAge - _iNum} ans"
+	if _iNum == iAge - _iNum
+		puts "Il y a #{_iNum} ans, tu avais la moitié de l'age que tu as aujourd'hui"
+	else
+		puts "Il y a #{_iNum} ans, tu avais #{iAge - _iNum} ans"
+	end
 end
