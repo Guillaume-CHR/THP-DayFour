4a63110d452c894b4d8889fda6250a9ff47c8dea
Guillaume_CH <guillaume.christe@gmail.com>
2019-01-04 01:26:12 +0100

    exo_05.rb

--- THP-exercice.rb
+++ THP-exercice.rb
@@ -1,1 +1,28 @@
-puts "Salut, ça farte ?
+#Affiche à l'écran la phrase 'On va compter le nombre d'heures de travail à THP'
+puts "On va compter le nombre d'heures de travail à THP"
+#Affiche à l'écran la phrase 'Travail : ' suivi de la multiplication de 10 par 5 par 60
+puts "Travail : #{10 * 5 * 11}"
+#Affiche à l'écran la phrase 'En minutes ça fait : ' suivi du résultat de la multiplication de 10 par 5 par 11 par 60 
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+#Affiche à l'écran la phrase 'Et en secondes ?'
+puts "Et en secondes ?"
+#Affiche à l'écran le résultat de la multiplication de 10 par 5 par 11 par 60 par 60
+puts 10 * 5 * 11 * 60 * 60
+#Affiche à l'écran la phrase 'Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?'
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+#Affiche à l'écran le résultat boolean de 3+2 est il inférieur à 5-7
+puts 3 + 2 < 5 - 7
+#Affiche à l'écran la phrase 'Ça fait combien 3 + 2 ? ' suivi du résultat de l'addition 3 + 2
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+#Affiche à l'écran la phrase 'Ça fait combien 5 - 7 ? ' suivi du résultat de la soustraction 5-7
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+#Affiche à l'écran la phrase 'Ok, c'est faux alors !'
+puts "Ok, c'est faux alors !"
+#Affiche à l'écran la phrase 'C'est drôle ça, faisons-en plus :'
+puts "C'est drôle ça, faisons-en plus :"
+#Affiche à l'écran la phrase 'Est-ce que 5 est plus grand que -2 ? ' suivi du résultat boolean 5 est il supérieur à -2
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+#Affiche à l'écran la phrase 'Est-ce que 5 est supérieur ou égal à -2 ? ' suivi du résultat boolean 5 est il supérieur ou égal à -2
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+#Affiche à l'écran la phrase 'Est-ce que 5 est inférieur ou égal à -2 ? ' suivi du résultat boolean 5 est il inférieur ou égal à -2
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
