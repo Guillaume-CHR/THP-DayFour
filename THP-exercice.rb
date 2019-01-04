d06bdc307da9d835633b8d81d5a36d0ddcfaa402
Guillaume_CH <guillaume.christe@gmail.com>
2019-01-04 11:44:15 +0100

    exo_19.rb

--- THP-exercice.rb
+++ THP-exercice.rb
@@ -2,4 +2,9 @@ tblEmail =[]
 for _iNum in (1..50)
 	tblEmail << "jean.dupont.#{_iNum.to_s.rjust(2,'0')}@email.fr"
 end
-puts tblEmail
+
+tblEmail.each do |_Email|
+	if (_Email.slice(13).to_i % 2) == 0 
+		puts _Email
+	end
+end
