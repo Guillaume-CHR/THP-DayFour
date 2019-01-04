7a522375904a3db321a6a0660612d359b691827d
Guillaume_CH <guillaume.christe@gmail.com>
2019-01-04 11:24:09 +0100

    exo_16.rb corrected

--- THP-exercice.rb
+++ THP-exercice.rb
@@ -2,6 +2,6 @@ puts "Bonjour, quel age as-tu ? "
 print "> "
 iAge = gets.chomp.to_i
 
-for _iNum in (iAge..0)
+for _iNum in (0..iAge)
 	puts "Il y a #{_iNum} ans, tu avais #{iAge - _iNum} ans"
 end
