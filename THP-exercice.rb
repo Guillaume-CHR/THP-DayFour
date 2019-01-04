f44a9ca2cc97702843908ae58be1e6fcdc9f02c3
Guillaume_CH <guillaume.christe@gmail.com>
2019-01-04 01:37:08 +0100

    exo_09.rb

--- THP-exercice.rb
+++ THP-exercice.rb
@@ -1,4 +1,7 @@
 puts "Bonjour, quel est ton prénom ?"
 print "> "
 user_name = gets.chomp
-puts "Bonjour #{user_name}"
+puts "Bonjour #{user_name}, et quel est ton nom de famille ?"
+print "> "
+user_lastname = gets.chomp
+puts "Bonjour #{user_name +' '+user_lastname}"
