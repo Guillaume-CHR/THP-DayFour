a83a2f57f92194b25f9537e7a627fddbbbc1e688
Guillaume_CH <guillaume.christe@gmail.com>
2019-01-04 11:34:42 +0100

    exo_18.rb

--- THP-exercice.rb
+++ THP-exercice.rb
@@ -1,11 +1,5 @@
-puts "Bonjour, quel age as-tu ? "
-print "> "
-iAge = gets.chomp.to_i
-
-for _iNum in (0..iAge)
-	if _iNum == iAge - _iNum
-		puts "Il y a #{_iNum} ans, tu avais la moitié de l'age que tu as aujourd'hui"
-	else
-		puts "Il y a #{_iNum} ans, tu avais #{iAge - _iNum} ans"
-	end
+tblEmail =[]
+for _iNum in (1..50)
+	tblEmail << "jean.dupont.#{_iNum.to_s.rjust(2,'0')}@email.fr"
 end
+puts tblEmail
